59e91785af47fbb19c29112069cb251c328c691f
wafflesJ <jacobsamuelmills7@gmail.com>
2025-03-01 18:13:10 -0500

    Update MyCode.cs

--- Assets/MyCode.cs
+++ Assets/MyCode.cs
@@ -7,7 +7,7 @@ public class MyCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Github");
     }
 
     // Update is called once per frame
